c81b95afd576ead1883dc027f5d7723084e682ca
Sky Miles <2188041-TheSorton@users.noreply.gitlab.com>
2020-06-09 14:58:56 -0700

    add unmute

---
+++ commands/admin/unmute.js
@@ -0,0 +1,32 @@
+const { Command } = require('discord.js-commando');
+const discord = require('discord.js');
+const config = require('../../config.json')
+
+
+module.exports = class Unmute extends Command {
+    constructor(client) {
+		super(client, {
+			name: 'unmute',
+			group: 'admin',
+			memberName: 'unmute',
+            description: "unmutes a user.",
+            args: [
+                {
+                    type:"user",
+                    prompt:"",
+                    key:"user",
+                }],
+                argsPromptLimit: 0
+		});
+	}
+    run(message, {user}) {
+        if (message.guild.member(user).roles.cache.some(role => role.name === 'silenced')) {
+        message.guild.member(user).roles.remove(config['Bot']['Mute'])
+        message.say(`**${user.tag}** has been unmuted.`)
+        }
+        else {
+            message.say(`**${user.tag}** is not muted.`)
+        }
+
+    }
+};
