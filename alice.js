0380d447a4303d1679eaed155bb707e77ee2e81b
Sky Miles <2188041-TheSorton@users.noreply.gitlab.com>
2020-06-09 10:53:09 -0700

    add main bot file

---
+++ alice.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const config = require('./config.json')
+const { CommandoClient } = require('discord.js-commando');
+const path = require('path');
+
+const client = new CommandoClient({
+	commandPrefix: config['Bot']['Prefix'],
+    owner: config['Bot']['Owner'],
+    disableEveryone: true
+});
+
+client.registry
+	.registerDefaultTypes()
+	.registerGroups([
+		['admin', 'Admin commands'],
+		['util', 'Utility commands'],
+	])
+	.registerDefaultGroups()
+	.registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+    
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+    client.user.setActivity('a!help');
+});
+    
+client.on('error', console.error);
+client.login(config['Bot']['Token']);
