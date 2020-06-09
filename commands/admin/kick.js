03fcf3cc40f336ca4dfdf12126f05e35afdfc0c4
Sky Miles <2188041-TheSorton@users.noreply.gitlab.com>
2020-06-09 11:40:47 -0700

    add kick

---
+++ commands/admin/kick.js
@@ -0,0 +1,25 @@
+const { Command } = require('discord.js-commando');
+const discord = require('discord.js');
+
+
+module.exports = class Kick extends Command {
+    constructor(client) {
+		super(client, {
+			name: 'kick',
+			group: 'admin',
+			memberName: 'kick',
+            description: "Kicks a user.",
+            args: [
+                {
+                    type:"user",
+                    prompt:"",
+                    key:"user",
+                }],
+                argsPromptLimit: 0
+		});
+	}
+    run(message, {user}) {
+        message.guild.member(user).kick()
+        message.say(`${user} has been kicked.`)
+    }
+};
