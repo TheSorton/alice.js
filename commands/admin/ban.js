3d83f0f5bda8ed819eeb57dc0f7c0766910de0cf
Sky Miles <2188041-TheSorton@users.noreply.gitlab.com>
2020-06-09 11:40:42 -0700

    add ban

---
+++ commands/admin/ban.js
@@ -0,0 +1,32 @@
+const { Command } = require('discord.js-commando');
+const discord = require('discord.js');
+
+
+module.exports = class Ban extends Command {
+    constructor(client) {
+		super(client, {
+			name: 'ban',
+			group: 'admin',
+			memberName: 'ban',
+            description: "Bans a user. If no reason is given, 'No reason given' is the reason",
+            args: [
+                {
+                    type: "user",
+                    prompt: "",
+                    key: "user"
+                },
+                {
+                    type: "string",
+                    prompt: "",
+                    key: "string",
+                    default: "No reason given"
+                }],
+                
+                argsPromptLimit: 0
+		});
+	}
+    run(message, { user, string }) {
+        message.guild.member(user).ban({reason: string})
+        message.say(`${user} has been banned.`)
+    }
+};
