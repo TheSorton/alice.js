6194a2e57a11b32c760ad433f2e3269859273c0f
Alan Miles <thesorton@icloud.com>
2020-06-18 11:00:44 -0700

    my laziness is showing

--- commands/admin/kick.js
+++ commands/admin/kick.js
@@ -1,9 +1,9 @@
 module.exports = {
     run: async(client, message, args) => {
 
-        if(!message.member.permissions.has("KICK_MEMBERS"))return message.channel.send(`You can't do that.`)
-        const gMember = message.guild.member(message.mentions.users.first())
-        await gMember.kick({ reason: args.slice(1).join(" ") } )
+        if(!message.member.permissions.has("KICK_MEMBERS"))return message.channel.send(`You can't do that.`);
+        const gMember = message.guild.member(message.mentions.users.first());
+        await gMember.kick();
 
     },
     name: 'kick',
