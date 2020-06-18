8387a4292f7b6eff2b445f1154a5d75f3fae0264
Alan Miles <thesorton@icloud.com>
2020-06-18 10:55:55 -0700

    add botinfo command

---
+++ commands/utilities/bot-info.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require('discord.js');
+const pkgInfo = require('../../package.json')
+const { helpers } = require('../../utils/helpers')
+
+module.exports = {
+    run: async(client, message, args) => {
+
+    var embed = new MessageEmbed()
+    .setAuthor(`${client.user.username}'s stats.`, client.user.avatarURL({format: 'png', dynamic: 'true' }))
+    .setThumbnail(client.user.avatarURL({format: 'png', dynamic: 'true', size: 2048 }))
+    .addFields(
+        {name: "Version", value: pkgInfo.version},
+        {name: "Node.JS Version", value: process.version},
+        {name: "Discord.JS Version", value: pkgInfo.dependencies["discord.js"].replace('^', 'v'), inline: false},
+        {name: "Uptime", value: helpers.format(process.uptime())}
+    )
+    await message.channel.send({ embed: embed})
+
+    },
+    name: 'botinfo',
+    category: 'utilities',
+    argsReq: false,
+    aliases: ['botstats'],
+    description: "Get's the bot's stats."
+}
