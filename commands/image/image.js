ef33d905c2126bab8501ecf948dd1042c513652d
FungiOfDeath <2402020+FungiOfDeath@users.noreply.github.com>
2020-06-21 17:28:16 -0400

    [#1] Generalize Reaction Tracking
    Generalizes tracking by including a Message Type field in storage.
    
    Modifies `commands/image/image.js` (`%image`) to use this new
    tracking mechanism. The older method has been removed.
    
    See #1.

--- commands/image/image.js
+++ commands/image/image.js
@@ -2,7 +2,8 @@
 const tiny = require('tiny-json-http')
 const config = require('../../config/config.json');
 const { MessageEmbed } = require('discord.js');
-const { embedData } = require('../../utils/embedData')
+const { track_message, is_message_tracked, MessageType }
+    = require('../../utils/reaction_tracking.js');
 
 module.exports = {
     run: async(client, message, args) => {
@@ -21,13 +22,15 @@ module.exports = {
                 .setImage(body.items[0].link)
                 .setDescription(`[${body.items[0].title}](${body.items[0].image.contextLink})`)
 
-                message.channel.send({ embed })
-                .then(msg => msg.react('⬅️').then(msg.react('➡️').then(msg.react('❌'))).then(embedData.storeEmbed(msg)))
-                i = 0
+                message.channel.send({ embed }).then(
+                    msg => msg.react('⬅️').then(
+                           msg.react('➡️').then(
+                           msg.react('❌').then(track_message(MessageType.Image, msg)))));
 
+                i = 0
                 client.on('messageReactionAdd', async (reaction, user) => {
                     if (user.bot) return;
-                    if (embedData.isReactionOnImageCmdEmbed(reaction.message)) {
+                    if (is_message_tracked(MessageType.Image, reaction.message)) {
                         if (user.id === message.author.id)  {
                             if (reaction._emoji.name === '➡️') {
                                 i = i + 1;
@@ -56,7 +59,7 @@ module.exports = {
 
                 client.on('messageReactionRemove', async (reaction, user) => {
                     if (user.bot) return;
-                    if (embedData.isReactionOnImageCmdEmbed(reaction.message)) {
+                    if (is_message_tracked(MessageType.Image, reaction.message)) {
                         if (user.id === message.author.id)  {
                             if (reaction._emoji.name === '➡️') {
                                 i = i + 1;
