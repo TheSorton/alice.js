01854bee168b52ed741db1852bdf83b787abdbc3
Alan <thesorton@icloud.com>
2020-07-06 11:06:17 -0700

    use collector

--- commands/google/youtube.js
+++ commands/google/youtube.js
@@ -8,6 +8,11 @@ const { track_message, is_message_tracked, MessageType }
 module.exports = {
     run: async(client, message, args) => {
 
+        const filter = (reaction, user) => {
+            return ['⬅️', '➡️',  '❌' ].includes(reaction.emoji.name) && user.id === message.author.id;
+
+        };
+
         let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&type=video&videoDefinition=high&key=${config.google.apikey}&q=${args.join(' ')}`
         tiny.get({url}, function _get(err, response) {
             if (err) {
@@ -18,63 +23,49 @@ module.exports = {
                 var body =  response.body;
                 if (!body.items)  return message.reply("No results found.")
                 link = `https://youtu.be/${body.items[0].id.videoId}`
+                i = 0
 
                 message.channel.send(link).then(
                     msg => msg.react('⬅️').then(
                            msg.react('➡️').then(
-                           msg.react('❌').then(track_message(MessageType.YouTube, msg)))));
+                           msg.react('❌').then(
+                msg.createReactionCollector(filter, { time: 60000, dispose: true })
+                .on('collect', reaction => {
+                    if (reaction.emoji.name === '⬅️' && 4 >= i > 0) {
+                        --i;
+                        reaction.message.edit(`https://youtu.be/${body.items[i].id.videoId}`)
+                    }
+                    else if (reaction.emoji.name === '➡️' && i <= 4) {
+                        ++i;
+                        if (i === 5) return
+                        reaction.message.edit(`https://youtu.be/${body.items[i].id.videoId}`)
 
-                i = 0
-                client.on('messageReactionAdd', async (reaction, user) => {
-                    if (user.bot) return;
-                    if (is_message_tracked(MessageType.YouTube, reaction.message)) {
-                        if (user.id === message.author.id)  {
-                            if (reaction._emoji.name === '➡️') {
-                                i = i + 1;
-
-                                link = `https://youtu.be/${body.items[i].id.videoId}`
-
-                                await reaction.message.edit(link)
-                            }
-                            else if (reaction._emoji.name === '⬅️' && i > 0) {
-                                --i;
-
-                                link = `https://youtu.be/${body.items[i].id.videoId}`
-
-                                await reaction.message.edit(link)
-                            }
-                            else if (reaction._emoji.name === '❌') {
-                                await reaction.message.delete()
-                            }
-                        }
+                    }
+                    else if (reaction.emoji.name === '❌') {
+                        msg.delete();
                     }
                 })
+                .on('remove', reaction => {
+                    if (reaction.emoji.name === '⬅️' && 4 >= i > 0) {
+                        --i;
+                        reaction.message.edit(`https://youtu.be/${body.items[i].id.videoId}`)
 
-                client.on('messageReactionRemove', async (reaction, user) => {
-                    if (user.bot) return;
-                    if (is_message_tracked(MessageType.YouTube, reaction.message)) {
-                        if (user.id === message.author.id)  {
-                            if (reaction._emoji.name === '➡️') {
-                                i = i + 1;
-
-                                link = `https://youtu.be/${body.items[i].id.videoId}`
-
-                                await reaction.message.edit(link)
-                            }
-                            else if (reaction._emoji.name === '⬅️' && i > 0) {
-                                --i;
-                                link = `https://youtu.be/${body.items[i].id.videoId}`
-
-                                await reaction.message.edit(link)
-                            }
-                            else if (reaction._emoji.name === '❌') {
-                                await reaction.message.delete()
-                            }
-                        }
+                    }
+                    else if (reaction.emoji.name === '➡️' && i < 5) {
+                        ++i;
+                        if (i === 5) return
+                        reaction.message.edit(`https://youtu.be/${body.items[i].id.videoId}`)
+                    }
+                    else if (reaction.emoji.name === '❌') {
+                        msg.delete();
                     }
                 })
-            }
-        });
+                .on('end', collected => {
+                    msg.reactions.removeAll()
+                })
+                ))))
+            }})
+
     },
     name: 'yt',
     category: 'google',
