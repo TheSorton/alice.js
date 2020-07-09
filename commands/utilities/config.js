a73ead4ec38a5daa1c4b3297a5703929fe48b28f
Alan <thesorton@icloud.com>
2020-07-08 19:43:31 -0700

    inital test

--- commands/utilities/config.js
+++ commands/utilities/config.js
@@ -4,10 +4,22 @@ const configModel = require("../../database/models/server")
 module.exports = {
     run: async(client, message, args) => {
         if(!message.member.permissions.has("MANAGE_SERVER")) return message.channel.send(`You can't do that.`)
-        let doc = await configModel
         switch(args[0]) {
-            case 'log':
-
+            case '-log':
+                if (!args[1]) await message.reply("Please specify a channel by its ID")
+                else {
+                    let doc = await configModel
+                    .findOne({ guildID: message.guild.id })
+                    .catch(err => console.log(err));
+                    if (doc) {
+                            doc.config.logChan = args[1]
+                            await doc.save();
+                            await message.channel.send('Log channel has been set.')
+                            return;
+                        }
+                }
+            default: 
+                await message.channel.send("What did you want to configure...?")
 
         }
 
@@ -16,5 +28,7 @@ module.exports = {
     name: 'config',
     category: 'utilities',
     args: true,
-    description: 'Configure the bot'
+    description: 'Configure the bot',
+    usage: "`heh`"
+    
 }
