cfe1cba044452e6ac1fcd672798979f1f0e74b84
Alan <thesorton@icloud.com>
2020-07-08 20:35:42 -0700

    i tried

--- log/guildMember.js
+++ log/guildMember.js
@@ -35,11 +35,11 @@ client.on('guildMemberUpdate', async (oldMember, newMember) => {
         }
     if (oldMember.nickname !== newMember.nickname) {
         let guild = configModel.findOne({ guildID: newMember.guild.id });
-        if (guild) let msgDoc = await messageModel.findOne({ guildID: id });
+        if (guild) { let msgDoc = await messageModel.findOne({ guildID: id }); 
         let { config } = msgDoc;
         let logChan = config.logChan 
         console.log(config, logChan)
-        oldMember.guild.channels.cache.find(x => x.id === logChan).send({embed: updateEmbed})
+        oldMember.guild.channels.cache.find(x => x.id === logChan).send({embed: updateEmbed})}
     }
     else {
         return
