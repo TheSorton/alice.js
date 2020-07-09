9db71c6df1dd609e928cfb8508cccbdf82e15218
Alan <thesorton@icloud.com>
2020-07-09 16:26:48 -0700

    make mute command dynamic

--- commands/admin/mute.js
+++ commands/admin/mute.js
@@ -1,15 +1,22 @@
+const configModel = require("../../database/models/server")
+
 module.exports = {
     run: async(client, message, args) => {
 
         if(!message.member.permissions.has("KICK_MEMBERS")) return message.channel.send(`You can't do that.`);
         const gMember = message.guild.member(message.mentions.users.first());
-        const role = message.guild.roles.cache.get('541726283176869918');
-        await gMember.roles.add(role);
-        await message.reply(`**${gMember.user.tag}** has been muted.`)
+        let guild = configModel.findOne({ guildID: message.guild.id });
+        if (guild) { 
+            let { config } = await configModel.findOne({ guildID: message.guild.id }); ;
+            let muteRole = config.muteRole 
+            const role = message.guild.roles.cache.find(x => x.id === muteRole);
+            await gMember.roles.add(role);
+            await message.reply(`**${gMember.user.tag}** has been muted.`)
+        }
 
     },
     name: 'mute',
-    category: 'admin',
+    category: 'admin',  
     aliases: ['silence', 'gag'],
     description: 'Mutes a guild member.',
     usage: '`mute <@User>`'
--- commands/admin/unmute.js
+++ commands/admin/unmute.js
@@ -1,11 +1,19 @@
+const configModel = require("../../database/models/server")
+
 module.exports = {
     run: async(client, message, args) => {
 
         if(!message.member.permissions.has("KICK_MEMBERS")) return message.channel.send(`You can't do that.`);
         const gMember = message.guild.member(message.mentions.users.first());
-        const role = message.guild.roles.cache.get('541726283176869918');
-        await gMember.roles.remove(role);
-        await message.reply(`**${gMember.user.tag}** has been unmuted.`)
+        let guild = configModel.findOne({ guildID: message.guild.id });
+        if (guild) { 
+            let { config } = await configModel.findOne({ guildID: message.guild.id }); ;
+            let muteRole = config.muteRole 
+            const role = message.guild.roles.cache.find(x => x.id === muteRole);
+            await gMember.roles.remove(role);
+            await message.reply(`**${gMember.user.tag}** has been unmuted.`)
+        }
+
 
     },
     name: 'unmute',
