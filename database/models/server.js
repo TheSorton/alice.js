97871ea6ead667f0f1af6272afa9a7962793169e
Alan <thesorton@icloud.com>
2020-07-08 19:59:50 -0700

    lets try a new schema

--- database/models/server.js
+++ database/models/server.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 const MessageScheme = new mongoose.Schema({
     guildID: { type: String, required: true },
-    config: { type: mongoose.SchemaTypes.Mixed }
+    config: { 
+        logChan: String,
+        welChan: String,
+        muteRole: String 
+    }
 })
 
 const ConfigModel = module.exports = mongoose.model('config', MessageScheme)
