3fa7f69e831221bd05aefd85ec795da8afef62f9
Alan <thesorton@icloud.com>
2020-07-10 11:31:56 -0700

    serverinfo command

---
+++ commands/utilities/server-info.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js")
+const { helpers } = require('../../utils/helpers')
+
+module.exports = {
+    run: async(client, message, args) => {
+        try {
+            roleArray = [];
+            message.guild.roles.cache.keyArray().forEach(x => {
+                roleArray.push(message.guild.roles.resolve(x))
+                roleArray.sort(helpers.sortArrayBy('rawPosition', true, parseInt))
+                if (roleArray.length > 15) {
+                    roles = `${roleArray.slice(0, 15).join(' ')} ...`
+                }
+                else {
+                    roles = roleArray.join(' ')
+                }
+            })
+
+            const embed = new MessageEmbed()
+            .setAuthor(`${message.guild.name}'s information`)
+            .setThumbnail(message.guild.iconURL({format: 'png', dynamic: true}))
+            .addFields(
+                { name: 'Server ID', value: message.guild.id, inline: true },
+                { name: 'Owner', value: `<@${message.guild.ownerID}>`, inline: true},
+                { name: 'Created on', value: message.guild.createdAt, inline: false},
+                { name: 'Partnered?_ _', value: message.guild.partnered, inline: true},
+                { name: 'Boost level_ _', value: message.guild.premiumTier, inline: true },
+                { name: '# of boosters', value: message.guild.premiumSubscriptionCount, inline: true },
+                { name: '# of members', value: message.guild.memberCount, inline: true},
+                { name: `Roles [${message.guild.roles.cache.keyArray().length - 1}]`, value: roles }
+
+            )
+            await message.channel.send(embed)
+
+        }
+        catch (error) {
+            await message.channel.send(`\`${error}\`\n You shouldn't see this. Contact alan ✨#1989`)
+        }
+    },
+    name: 'serverinfo',
+    category: 'utilities',
+    args: false,
+    aliases: ['guild', 'server'],
+    description: "Displays the server's information"
+}
