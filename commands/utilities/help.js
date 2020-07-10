fa1e7783721b5caeaea3d76b0be2aec7e38c6fab
Alan <thesorton@icloud.com>
2020-07-09 19:40:05 -0700

    change how help command displays

--- commands/utilities/help.js
+++ commands/utilities/help.js
@@ -19,18 +19,36 @@ module.exports = {
             .setColor("RANDOM")
             .setAuthor(`${config.bot.prefix}help [command] for more information on a command`)
     
-        const commands = (category) => {
-            return client.commands
-                .filter(cmd => cmd.category === category)
-                .map(cmd => `- \`${cmd.name}\``)
-                .join("\n");
-        }
-    
-        const info = client.categories
-            .map(cat => stripIndents`**${cat[0].toUpperCase() + cat.slice(1)}** \n${commands(cat)}`)
-            .reduce((string, category) => string + "\n" + category);
-    
-        return message.channel.send(embed.setDescription(info));
+            // Format the name of a category
+            const category_name_fmt = cat => cat[0].toUpperCase() + cat.slice(1);
+
+            // Create an array of commands (from client.command) to an array of
+            // groups describing a category + its commands
+            const group_cmd_into_categories = cmds => {
+                const ret = {};
+                const add_to_ret = cmd => {
+                    if (ret[cmd.category] === undefined)
+                        ret[cmd.category] = [];
+                    
+                    ret[cmd.category].push(cmd.name);
+                };
+                cmds.forEach(cmd => add_to_ret(cmd));
+                return ret;
+            };
+
+            // Given a grouped category + commands, format into an array of field
+            const category = (cat, cmds) => {
+                const description = cmds.join(", ")
+                const category_name = `**— ${category_name_fmt(cat)} —**`;
+                return { name: category_name, value: description, inline: true };
+            }
+
+            const groups = group_cmd_into_categories(client.commands);
+            const keys   = Object.keys(groups);
+            const fields = keys.map(key => category(key, groups[key]));
+
+            embed.addFields(...fields)
+            return message.channel.send(embed);
     }
     
     function getCMD(client, message, input) {
