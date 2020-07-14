ba47fe7cc8d35a89d8cecbb7e5614698deb257c4
Alan Miles <thesorton@icloud.com>
2020-07-14 08:42:35 -0700

    scheduling works but holy fuck

--- commands/utilities/remind.js
+++ commands/utilities/remind.js
@@ -1,12 +1,11 @@
 
 const { clean_everyone, clean_here } = require("../../utils/sanitize.js").sanitize;
+var schedule = require('node-schedule');
 
 module.exports = {
     run: async (client, message, args) => {
         try {
-            const Send = (args) => {
-                console.log(`${args}`);
-            }
+
             var args = Array.from(args.join(' ').split(" in "))
             var reminder = args[0]
             var time = Array.from(args.slice(1).join(' ').split(' in ').join(' ').split(', ').join(' ').split(' '))
@@ -16,21 +15,33 @@ module.exports = {
             }
 
             var x = [];
-            for (let i = 0; i < objs.length; i += 4 ) {
-                switch (objs[i].unit) {
-                case 'day': 
+            console.log(objs.length)
+            for (let i = 0; i < objs.length; i++) {
+                if (objs[i].unit === 'day') {
                     x.push(Number(objs[i].number) * 86400000)
-                case 'hour':
+                    console.log(objs[i].unit)
+                }
+                else if (objs[i].unit === 'hour') {
                     x.push(Number(objs[i].number) * 3600000)
-                case 'minute':
+                    console.log(objs[i].unit)
+                }
+                else if (objs[i].unit === 'minute') {
                     x.push(Number(objs[i].number) * 60000)
-                case 'second':
+                    console.log(objs[i].unit)
+                }
+                else if (objs[i].unit === 'second') {
                     x.push(Number(objs[i].number) * 1000)
-                }}
+                    console.log(objs[i].unit)
+                }
+            }
 
             const timeout = x.reduce((a, b) => a + b, 0)
-            console.log(objs + '\n' + x)
-            setTimeout(Send, timeout, reminder);
+            const schedule = Math.floor(Date.now()) + timeout
+            const datetime = new Date(schedule)
+
+            schedule.scheduleJob(schedule, function(){
+                message.reply(reminder)
+            });
         }
         catch (error) {
             await message.channel.send(`\`${error}\`\n You shouldn't see this. Contact alan ✨#1989`)
