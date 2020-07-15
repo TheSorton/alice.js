7286a591c1fe9fce9f8abf8610e6efb4f9e8e345
Alan <thesorton@icloud.com>
2020-07-14 20:16:14 -0700

    check for arguments, check for unit(s)

--- commands/utilities/remind.js
+++ commands/utilities/remind.js
@@ -10,6 +10,9 @@ module.exports = {
             var args = Array.from(args.join(' ').split(" in "))
             var reminder = args[0]
             var time = Array.from(args.slice(1).join(' ').split(' in ').join(' ').split(', ').join(' ').split(' '))
+
+            if (!reminder) return await message.reply("you didn't tell me what you wanted to be reminded of.")
+
             const objs = [];
             for (let i = 0; i < time.length; i += 2) {
                 objs.push({ number: time[i], unit: time[i + 1] });
@@ -17,19 +20,23 @@ module.exports = {
 
             var x = [];
             for (let i = 0; i < objs.length; i++) {
-                if (objs[i].unit === 'day') {
+                if (objs[i].unit === 'day' || objs[i].unit === 'days') {
                     x.push(Number(objs[i].number) * 86400000)
                 }
-                else if (objs[i].unit === 'hour') {
+                else if (objs[i].unit === 'hour' || objs[i].unit === 'hours') {
                     x.push(Number(objs[i].number) * 3600000)
                 }
-                else if (objs[i].unit === 'minute') {
+                else if (objs[i].unit === 'minute' || objs[i].unit === 'minutes') {
                     x.push(Number(objs[i].number) * 60000)
                 }
-                else if (objs[i].unit === 'second') {
+                else if (objs[i].unit === 'second' || objs[i].unit === 'seconds' ) {
                     x.push(Number(objs[i].number) * 1000)
                 }
+                else {
+                    return await message.reply("you didn't tell me when you wanted to be reminded.")
+                }
             }
+            console.log(time)
 
             const timeout = x.reduce((a, b) => a + b, 0)
             const date = Math.floor(Date.now())
@@ -67,5 +74,5 @@ module.exports = {
     args: true,
     aliases: ['remindme'],
     description: 'Reminds you of something',
-    usage: '`remind <reminder> in <time>`'
+    usage: '```markdown\n# Remind command\n# Usage\nremind <reminder> in <time>\n# Time\nTime is in the format x day(s), y hour(s), z minutes(s), s second(s)```'
 }
