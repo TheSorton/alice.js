7b6db25f0d478e3633a253250cbef6dd49c94777
Alan Miles <thesorton@icloud.com>
2020-10-20 17:38:58 -0700

    roll command update

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -1,9 +1,14 @@
 const { helpers } = require('../../utils/helpers')
+const config = require('../../config/config.json')
 
 module.exports = {
   run: async(client, message, args) => {
     try {
-      message.reply(`You rolled ${helpers.rollDice()}`)
+      if (args[0]) {
+        let die = args[0].replace(/[^1-9]/,"");
+        await message.reply(`You rolled ${helpers.rollDice(die)}`)
+      }
+      else return message.reply(`You rolled ${helpers.rollDice(6)}`)
     }
     catch (error) {
       await message.channel.send(`\`${error}\``)
@@ -12,5 +17,6 @@ module.exports = {
   name: 'roll',
   category: 'fun',
   aliases: ['dice'],
-  description: 'Rolls a dice.'
+  description: 'Rolls a dice. Defaults to 6.',
+  usage: `\`${config.bot.prefix}roll [dice-size (for example d145)]\``
 }
