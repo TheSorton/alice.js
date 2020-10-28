faf64fe2748d1603c357cdf52c5c66b0046d6359
Alan Miles <thesorton@icloud.com>
2020-10-28 09:25:50 -0700

    Revert "global flag"
    This reverts commit 974b6fa085d1b552c7eaae8ca30666407dfbb16f.

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -5,7 +5,7 @@ module.exports = {
   run: async(client, message, args) => {
     try {
       if (args[0]) {
-        let die = args[0].replace(/[^1-9]/g,"");
+        let die = args[0].replace(/[^1-9]/,"");
         await message.reply(`You rolled ${helpers.rollDice(die)}`)
       }
       else return message.reply(`You rolled ${helpers.rollDice(6)}`)
