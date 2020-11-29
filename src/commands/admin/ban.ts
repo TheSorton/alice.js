ac6a5274d9c0b146bb5dff8e66268d242055ff5a
Alan Miles <nyx@lovelain.net>
2020-11-29 11:20:37 -0800

    add ban

---
+++ src/commands/admin/ban.ts
@@ -0,0 +1,16 @@
+import {Message} from 'discord.js'
+import * as config from '../../../config/config.json';
+const prefix = config.bot.prefix;
+
+module.exports = {
+  name: 'ban',
+  description: 'Bans a member',
+  usage: `${prefix}ban @User [Reason]`,
+  run(message: Message, args: string[] ) {
+    if (!message.member.permissions.has("BAN_MEMBERS")) return message.reply(`You can't do that.`);
+    if (message.mentions.users.first()) {
+      const gMember = message.guild.member(message.mentions.users.first())
+      gMember.ban({ reason: args.slice(1).join(" ") } )
+    }
+  }
+};
