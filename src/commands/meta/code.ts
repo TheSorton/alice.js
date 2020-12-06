9b68d7fff7bd6a2eb820a530d3e80ea9c995bcad
Alan Miles <nyx@lovelain.net>
2020-12-06 10:45:10 -0800

    add aliases to code

--- src/commands/meta/code.ts
+++ src/commands/meta/code.ts
@@ -1,9 +1,9 @@
 import {Message} from 'discord.js';
-import { prefix } from '../../lib/aliceClient';
 
 module.exports = {
   name: 'code',
   description: 'Get a link to the bot\'s GitHub.',
+  aliases: ['src', 'github', 'source'],
   run(message: Message) {
     message.reply('https://github.com/thesorton/alice/tree/typescript/')
   },
