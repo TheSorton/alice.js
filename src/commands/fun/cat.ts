177ab0d1ee143f2afca9435c284894e6fa15e4dc
Alan Miles <thesorton@icloud.com>
2023-08-01 19:00:23 -0700

    fix cat command

--- src/commands/fun/cat.ts
+++ src/commands/fun/cat.ts
@@ -1,8 +1,7 @@
 import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import config from "../../../config/config.json";
-import bent from "bent";
+import axios from "axios";
 import emoji from "node-emoji";
-const getJSON = bent("json");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,11 +13,11 @@ module.exports = {
         return pronoun.charAt(0) === "h" ? "They" : "they";
       });
     };
-    const search = await getJSON(
+    const search = await axios.get(
       `https://api.thecatapi.com/v1/images/search?limit=1&has_breeds=1&api_key=${config.cat.apikey}`
     );
     const embed = new EmbedBuilder();
-    const result = search[0];
+    const result = search.data[0];
     const breed = result.breeds[0];
     const emojiChar = emoji.get(`flag-${breed.country_code.toLowerCase()}`);
     const url = result.url;
